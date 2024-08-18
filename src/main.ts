f315f769a589dcb1ab9b50c526832c8dc47857bf
swagwangjie <18721895348@sina.cn>
2024-08-18 11:13:29 +0800

    去除冗余文件

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +9,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
